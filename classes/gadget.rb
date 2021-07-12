ce1a1b49e46bb1e36892658cec4c23ba38744161
Avilee Williams <avilee.williams@gmail.com>
2021-07-12 14:37:37 -0500

    Making modules

--- classes/gadget.rb
+++ classes/gadget.rb
@@ -1,21 +1,30 @@
 class Gadget
-    def initialize
-        @username = "User #{rand(1..100)}"
-        @password = "topsecret"
+    
+    attr_accessor :username
+    attr_reader :production_number
+    attr_writer :password
+    
+    def initialize(username, password)
+        @username = username
+        @password = password
         @production_number = "#{("a".."z").to_a.sample}-#{rand(1..999)}"
     end
 
-    def info
-        "Gadget #{@production_number} has the username #{@username}"
+    def to_s
+        "Gadget #{@production_number} has the username #{@username}. It is made from the #{self.class} class and it has the ID #{self.object_id}"
     end
 end
 
+g1 = Gadget.new("dw_402", "c00lc0de")
+g1.username = "yayaya"
 
-phone = Gadget.new
-laptop = Gadget.new
-
-p phone.info
-p laptop.info
-
+p g1.username
+p g1.production_number
 
 
+g2 = Gadget.new("cool.user", "av999")
+g3 = Gadget.new("iminlovewith", "JOHNCENAAAAA")
+
+p g1.to_s
+p g2.to_s
+p g3.to_s
