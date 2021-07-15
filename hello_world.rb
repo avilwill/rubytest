d8074dcfca45debc213d819240ca6cdd2b5147c0
Avilee Williams <avilee.williams@gmail.com>
2021-07-15 15:33:24 -0500

    structs

--- hello_world.rb
+++ hello_world.rb
@@ -1,3 +1,16 @@
-require 'time'
+module AppStore
+    
+    App = Struct.new(:name, :developer, :version)
 
-puts Time.strptime("03-04-2000", "%m-%d-%Y")
+    APPS = [
+        App.new(:Chat, :facebook, 2.0),
+        App.new(:Twitter, :twitter, 5.8),
+        App.new(:Weather, :yahoo, 10.15)
+    ]
+
+    def self.find_app(name)
+        APPS.find {|value| value.name == name}
+        
+    end
+
+end
