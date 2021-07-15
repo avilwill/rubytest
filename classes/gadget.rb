eead3263c8856974e7e4251cf401f14e64c69343
Avilee Williams <avilee.williams@gmail.com>
2021-07-15 09:07:08 -0500

    main file

--- classes/gadget.rb
+++ classes/gadget.rb
@@ -1,30 +1,29 @@
-class Gadget
-    
-    attr_accessor :username
-    attr_reader :production_number
-    attr_writer :password
-    
-    def initialize(username, password)
-        @username = username
-        @password = password
-        @production_number = "#{("a".."z").to_a.sample}-#{rand(1..999)}"
+class ConvenienceStore
+    include Enumerable
+    attr_reader :snacks
+
+    def initialize
+        @snacks = []
     end
 
-    def to_s
-        "Gadget #{@production_number} has the username #{@username}. It is made from the #{self.class} class and it has the ID #{self.object_id}"
+    def add_snack(snack)
+        snacks << snack
+    end
+
+    def each
+        snacks.each do |value|
+            yield value
+        end
     end
 end
 
-g1 = Gadget.new("dw_402", "c00lc0de")
-g1.username = "yayaya"
-
-p g1.username
-p g1.production_number
-
-
-g2 = Gadget.new("cool.user", "av999")
-g3 = Gadget.new("iminlovewith", "JOHNCENAAAAA")
-
-p g1.to_s
-p g2.to_s
-p g3.to_s
+bodega = ConvenienceStore.new
+bodega.add_snack("soup")
+bodega.add_snack("cookies")
+bodega.add_snack("crack")
+bodega.add_snack("reeses")
+bodega.add_snack("granola")
+bodega.each {|value| puts "#{value} is delicious!"}
+p bodega.map {|value| value.upcase}
+p bodega.reject { |value| value.downcase.include?("o")}
+p bodega.sort
---
+++ classes/project/main.rb
@@ -0,0 +1,3 @@
+module PurchaseMod
+    
+end
