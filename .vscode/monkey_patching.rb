a239a7aa5b1a6d8db3c0b9dc0ab92113c92bbd4d
Avilee Williams <avilee.williams@gmail.com>
2021-07-15 16:22:33 -0500

    monkey ppatching w/ hash

--- .vscode/monkey_patching.rb
+++ .vscode/monkey_patching.rb
@@ -1,9 +1,20 @@
-class Array 
-    def sum
-        total = 0
-        self.each {|value| total += value if value.is_a?(Numeric)}
-        total
+class Hash
+    def indetify_duplicate_values
+        values = []
+        dupes = []
+
+        self.each_value do |value|
+            if values.include?(value)
+                dupes << value
+            else
+                values << value
+            end
+        end
+        dupes.uniq
     end
 end
 
-p [1, "hello", 2, 3, false, 4].sum
+scores = {a: 100, b: 100, c: 83, d: 50, e: 13,
+         f: 6, g: 100, h: 13, i: 50, j: 80}
+
+p scores.indetify_duplicate_values
